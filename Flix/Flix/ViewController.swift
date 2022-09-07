089b8be24b8be439ef18679b74970f67384d7805
angel zambrano <angeldzambrano99@gmail.com>
2022-09-06 19:24:01 -0700

    added the set up for app

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Angel Zambrano on 9/6/22.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        title = "Now Playing"
+    }
+
+
+}
+
