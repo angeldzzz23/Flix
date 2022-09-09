25dd62c87dc64ec6979c48ec8f3b7610e44e8e6a
angel zambrano <angeldzambrano99@gmail.com>
2022-09-08 21:23:10 -0700

    added titleLbl

--- Flix/Flix/MovieTableViewCell.swift
+++ Flix/Flix/MovieTableViewCell.swift
@@ -13,7 +13,31 @@ class MovieTableViewCell: UITableViewCell {
     //
     
     static let identifier = "MovieTableViewCell"
-   private let imageview = UIImageView() // width = 89
+    private let imageview = UIImageView() // width = 89
+    
+    private let titleLbl: UILabel = {
+        let titleLbl = UILabel()
+        titleLbl.text = "Luca"
+        titleLbl.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        return titleLbl
+    }()
+    
+    
+    
+    private let descriptionLbl: UILabel = {
+        let lbl = UILabel()
+        return lbl
+    }()
+
+    
+    func configureImage(image: UIImage) {
+        self.imageview.image = image
+    }
+    
+    func configuretitle(text: String) {
+        self.titleLbl.text = text
+    }
+    
     
     
     override func awakeFromNib() {
@@ -37,15 +61,28 @@ class MovieTableViewCell: UITableViewCell {
         imageview.backgroundColor = .blue
         contentView.backgroundColor = .red
         
+        contentView.addSubview(titleLbl)
+        titleLbl.translatesAutoresizingMaskIntoConstraints = false
+        
     }
     
     private func setAutoLayout() {
+        // setting constraints for the image view
         NSLayoutConstraint.activate([
             imageview.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
             imageview.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             imageview.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
             imageview.widthAnchor.constraint(equalToConstant: 90)
         ])
+        
+        // setting constraints for the titleLbl
+        NSLayoutConstraint.activate([
+            titleLbl.leadingAnchor.constraint(equalTo: imageview.trailingAnchor, constant: 10),
+            titleLbl.topAnchor.constraint(equalTo: imageview.topAnchor, constant: 0),
+            titleLbl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ])
+        
+        
     }
     
     required init?(coder: NSCoder) {
