de8abce0d99472e44fc8f1fbd7141c94e8451b42
angel zambrano <angeldzambrano99@gmail.com>
2022-09-10 20:27:12 -0700

    fixed the flix app

---
+++ Flix/Flix/Model/LaunchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchViewController.swift
+//  Flix
+//
+//  Created by Angel Zambrano on 9/10/22.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
